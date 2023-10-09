739fa995aa43be2cbdca147045581f1e46e72fa5
ianscottmartin <ianscottmartin@gmail.com>
2023-10-09 15:27:38 -0600

    more errs

--- server/seed.py
+++ server/seed.py
@@ -39,7 +39,7 @@ with app.app_context():
     
     
     reviews = [
-        Review(content=faker.paragraph(), artist=artists[i].id)
+        Review(content=faker.paragraph(), artist_id=artists[i].id)
         for i in range(10)
     ]
 
