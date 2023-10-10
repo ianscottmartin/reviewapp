704ba827d3ccb786a33798049c7bda8ba561028f
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 13:11:43 -0600

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import HomePage from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/artists" component={Artists} />
+      <Route path="/museums" component={Museums} />
+      <Route path="/reviews" component={Reviews} />
+      <Route path="/users" component={Users} />
       
-      <HomePage />
+      
+      </Switch>
+      
     </div>
   );
 }
