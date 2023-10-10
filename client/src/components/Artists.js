ac5bde5ea4986a949f3fee5a9a46e3fe6d2f83be
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 12:47:20 -0600

    errs on artists

--- client/src/components/Artists.js
+++ client/src/components/Artists.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
-function Artist(){
+
+function Artist() {
+    const [artists, setArtists]= useState([]);
+
+    useEffect(()=> {
+
+        fetch("http://localhost:5000/api/artists")
+        .then((response) => response.json())
+        .then((data) => {
+            setArtists(data);
+        })
+    });
+    .catch((error) => {
+        console.error("Error fetching artist data", error);
+    });
+
+}, []);
+
     return (
         <div> 
-        <h2>Artists</h2>
+            <h2>Artists</h2>
+            <ul>
+                {artists.map((artist) => (
+                    <li key={artist.id}>{artist.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
