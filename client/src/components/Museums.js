88b2c0c2fd5b1d69d66d07469e239bdecea0d5a1
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 12:25:20 -0600

    users, reviews, artists, mjusuems

---
+++ client/src/components/Museums.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Museum(){
+    return (
+        <div> 
+        <h2>Museums</h2>
+        </div>
+    );
+}
+
+export default Museums();
---
+++ client/src/components/Reviews.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Review(){
+    return (
+        <div>
+        <h2>Reviews</h2>
+        </div>
+    );
+}
+
+export default Review();
