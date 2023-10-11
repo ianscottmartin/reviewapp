5032960a8aa2fad3dcd84199c7c6e2d09bd5f636
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 23:47:08 -0600

    editing but too tired

--- client/src/Artists.js
+++ client/src/Artists.js
@@ -1,13 +1,12 @@
-// Artists.js
-
 import React, { useState, useEffect } from "react";
 import withNavbar from "./Layout";
 
 function Artists() {
     const [artists, setArtists] = useState([]);
     const [artistName, setArtistName] = useState("");
-    const [artistWork, setArtistWork] = useState(""); 
-    const [artistDescription, setArtistDescription] = useState(""); 
+    const [artistWork, setArtistWork] = useState("");
+    const [artistDescription, setArtistDescription] = useState("");
+    const [selectedArtist, setSelectedArtist] = useState(null);
 
     useEffect(() => {
         // Fetch the list of artists when the component mounts
@@ -17,36 +16,111 @@ function Artists() {
             .catch((error) => console.error("Error fetching artists:", error));
     }, []);
 
+    // Define a function to clear the input fields
+    const clearInputFields = () => {
+        setArtistName("");
+        setArtistWork("");
+        setArtistDescription("");
+    };
+
+    // Define a function to handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Create an artist with the data
-        const newArtist = {
-            name: artistName,
-            work: artistWork, 
-            description: artistDescription, 
-        };
+        if (selectedArtist) {
+            // Editing an existing artist
+            const updatedArtist = {
+                id: selectedArtist.id,
+                name: artistName,
+                work: artistWork,
+                description: artistDescription,
+            };
 
-        // Send a POST to add the artist
-        fetch("/api/artists", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newArtist),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Artist added:", data);
-                
-                // After adding the artist
-                // Fetch the updated list of artists and update the state
-                fetch("/api/artists")
-                    .then((response) => response.json())
-                    .then((data) => setArtists(data))
-                    .catch((error) => console.error("Error fetching artists:", error));
+            // Send a PUT request to update the artist
+            fetch(`/api/artists/${selectedArtist.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(updatedArtist),
             })
-            .catch((error) => console.error("Error adding artist:", error));
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Artist updated:", data);
+
+                    // Reset form fields and selected artist
+                    setSelectedArtist(null);
+                    clearInputFields();
+
+                    // Fetch the updated list of artists and update the state
+                    fetch("/api/artists")
+                        .then((response) => response.json())
+                        .then((data) => setArtists(data))
+                        .catch((error) => console.error("Error fetching artists:", error));
+                })
+                .catch((error) => console.error("Error updating artist:", error));
+        } else {
+            // Creating a new artist
+            const newArtist = {
+                name: artistName,
+                work: artistWork,
+                description: artistDescription,
+            };
+
+            // Send a POST request to add the artist
+            fetch("/api/artists", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newArtist),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Artist added:", data);
+
+                    // Reset form fields
+                    clearInputFields();
+
+                    // Fetch the updated list of artists and update the state
+                    fetch("/api/artists")
+                        .then((response) => response.json())
+                        .then((data) => setArtists(data))
+                        .catch((error) => console.error("Error fetching artists:", error));
+                })
+                .catch((error) => console.error("Error adding artist:", error));
+        }
+    };
+
+    // Define a function to handle editing an artist
+    const handleEdit = (artist) => {
+        setSelectedArtist(artist);
+        setArtistName(artist.name);
+        setArtistWork(artist.work);
+        setArtistDescription(artist.description);
+    };
+
+    //Function delete an artist
+    const handleDelete = (artistId) => {
+        if (window.confirm("Are you sure you want to delete this artist?")) {
+            fetch(`/api/artists/${artistId}`, {
+                method: "DELETE",
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        // Artist deleted successfully, update the list
+                        fetch("/api/artists")
+                            .then((response) => response.json())
+                            .then((data) => setArtists(data))
+                            .catch((error) => console.error("Error fetching artists:", error));
+                    } else if (response.status === 404) {
+                        console.error("Artist not found.");
+                    } else {
+                        console.error("Error deleting artist.");
+                    }
+                })
+                .catch((error) => console.error("Error deleting artist:", error));
+        }
     };
 
     return (
@@ -55,13 +129,16 @@ function Artists() {
             {/* List of artists */}
             <ul>
                 {artists.map((artist) => (
-                    <li key={artist.id}>{artist.name}</li>
+                    <li key={artist.id}>
+                        {artist.name}
+                        <button onClick={() => handleEdit(artist)}>Edit</button>
+                        <button onClick={() => handleDelete(artist.id)}>Delete</button>
+                    </li>
                 ))}
             </ul>
 
-           
             <div>
-                <h2>Add an Artist</h2>
+                <h2>{selectedArtist ? "Edit Artist" : "Add an Artist"}</h2>
                 <form onSubmit={handleSubmit}>
                     <label>
                         Artist Name:
@@ -72,7 +149,7 @@ function Artists() {
                         />
                     </label>
                     <label>
-                        Artist Work: {/* Input for artist's work */}
+                        Artist Work:
                         <input
                             type="text"
                             value={artistWork}
@@ -80,18 +157,16 @@ function Artists() {
                         />
                     </label>
                     <label>
-                        Artist Description: {/* Input for artist's description */}
+                        Artist Description:
                         <input
                             type="text"
                             value={artistDescription}
                             onChange={(e) => setArtistDescription(e.target.value)}
                         />
                     </label>
-
-
-
-
-                    <button type="submit">Add Artist</button>
+                    <button type="submit">
+                        {selectedArtist ? "Update Artist" : "Add Artist"}
+                    </button>
                 </form>
             </div>
         </div>
