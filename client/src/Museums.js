9e524a40d707da5ae8258cbf0fdc86c83f2dd063
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 22:24:02 -0600

    reviews in console only

--- client/src/Museums.js
+++ client/src/Museums.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import withNavbar from "./Layout";
 
 function Museums() {
+    const [museums, setMuseums] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('api/museums')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setMuseums(data);
+            })
+            .catch((err) => {
+                console.error(err);
+                setError(err);
+            });
+    }, []);
+
+    if (error) {
+        return (
+            <div>
+                <h2>Museums</h2>
+                <p>An error occurred while fetching data: {error.message}</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h2>Museums</h2>
+            <ul>
+                {museums.map((museum) => (
+                    <li key={museum.id}>{museum.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
--- client/src/Reviews.js
+++ client/src/Reviews.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import withNavbar from "./Layout";
 
 function Reviews() {
+    const [reviews, setReviews] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('api/reviews')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data); // Check the data
+                setReviews(data);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setError(err);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div>
+                <h2>Loading...</h2>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                <h2>Reviews</h2>
+                <p>An error occurred while fetching data: {error.message}</p>
+            </div>
+        );
+    }
+
+    if (reviews.length === 0) {
+        return (
+            <div>
+                <h2>Reviews</h2>
+                <p>No reviews available.</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h2>Reviews</h2>
+            <ul>
+                {reviews.map((review) => (
+                    <li key={review.id}>{review.text}</li>
+                ))}
+            </ul>
         </div>
     );
 }
