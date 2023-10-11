87a8863c75d5a4c89f0589d3005f676f472f9f00
ianscottmartin <ianscottmartin@gmail.com>
2023-10-10 22:13:54 -0600

    have artists on page

--- client/src/Artists.js
+++ client/src/Artists.js
@@ -1,25 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import withNavbar from "./Layout";
 
 function Artists() {
-    // const [artists, setArtists]= useState([]);
+    const [artists, setArtists] = useState([]);
+    const [error, setError] = useState(null);
 
-    // useEffect(()=> {
-    //     fetch("http://localhost:5000/api/artists")
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //         setArtists(data);
-    //          })
-    //         .catch((error) => {
-    //             console.error("Error fetching artist data", error);
-    //         });
+    useEffect(() => {
+        fetch('api/artists')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setArtists(data);
+            })
+            .catch((err) => {
+                console.error(err);
+                setError(err);
+            });
+    }, []);
 
-    // }, []);
+    if (error) {
+        return (
+            <div>
+                <h2>Artists</h2>
+                <p>An error occurred while fetching data: {error.message}</p>
+            </div>
+        );
+    }
 
     return (
         <div>
             <h2>Artists</h2>
-
+            <ul>
+                {artists.map((artist) => (
+                    <li key={artist.id}>{artist.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
