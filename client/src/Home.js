616770a3b3a8367e8392a13772b647c0de1575ec
ianscottmartin <ianscottmartin@gmail.com>
2023-10-16 10:25:57 -0600

    ad to home

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,34 +4,41 @@ import SignUpForm from "./SignUpForm";
 import withNavbar from "./Layout";
 import "./css/home/Home.css";
 
-
-
 function Home() {
-    const [signInData, setSignInData] = useState(null);
-    const [signUpData, setSignUpData] = useState(null);
+  const [signInData, setSignInData] = useState(null);
+  const [signUpData, setSignUpData] = useState(null);
 
-    
-    const handleSignInSubmit = (data) => {
-        setSignInData(data);
-    };
+  const handleSignInSubmit = (data) => {
+    setSignInData(data);
+  };
 
-    const handleSignUpSubmit = (data) => {
-        setSignUpData(data);
-    };
+  const handleSignUpSubmit = (data) => {
+    setSignUpData(data);
+  };
 
-    return (
-        <div className="home">
-            <div className="home-background">
-          
-            </div>
-            <div className="auth-section">
-                <SignInForm onSubmit={handleSignInSubmit} />
-                <SignUpForm onSubmit={handleSignUpSubmit} />
-            </div>
-            {signInData && <div>Sign-In Data: {JSON.stringify(signInData)}</div>}
-            {signUpData && <div>Sign-Up Data: {JSON.stringify(signUpData)}</div>}
-        </div>
-    );
+  return (
+    <div className="home">
+      <div className="home-background">
+      
+      </div>
+
+      <div className="welcome-paragraph">
+        <h2>Welcome to Our Art Gallery</h2>
+        <p>
+          Thank you for visiting our art gallery website. We showcase a diverse
+          collection of artworks from talented artists around the world. Explore
+          the beauty of art and discover the creativity of our artists.
+        </p>
+      </div>
+
+      <div className="auth-section">
+        <SignInForm onSubmit={handleSignInSubmit} />
+        <SignUpForm onSubmit={handleSignUpSubmit} />
+      </div>
+      {signInData && <div>Sign-In Data: {JSON.stringify(signInData)}</div>}
+      {signUpData && <div>Sign-Up Data: {JSON.stringify(signUpData)}</div>}
+    </div>
+  );
 }
 
 export default withNavbar(Home);
