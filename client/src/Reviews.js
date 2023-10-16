f0af53d14c4889cb51c284886727a6330f50e60d
ianscottmartin <ianscottmartin@gmail.com>
2023-10-16 10:16:01 -0600

    css errors

--- client/src/Reviews.js
+++ client/src/Reviews.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import withNavbar from "./Layout";
 import "../src/css/review/Reviews.css";
+import withNavbar from "./Layout";
 
 function Reviews() {
   const [newReview, setNewReview] = useState("");
   const [selectedArtistId, setSelectedArtistId] = useState("");
-  const [artists, setArtists] = useState([]); 
+  const [artists, setArtists] = useState([]);
   const [reviews, setReviews] = useState([]);
+  const [editingReview, setEditingReview] = useState(null); // Track the review being edited
 
   useEffect(() => {
-    // Fetch the list of artists
+    
     fetch("/api/artists")
       .then((response) => response.json())
       .then((data) => {
@@ -19,7 +20,7 @@ function Reviews() {
         console.error("Error fetching artists:", error);
       });
 
-    // Fetch the list of reviews
+    
     fetchReviews();
   }, []);
 
@@ -40,22 +41,70 @@ function Reviews() {
       return;
     }
 
-    fetch("/api/reviews", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ content: newReview, artist_id: selectedArtistId }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Review added:", data);
-        setNewReview("");
-        fetchReviews();
+    if (editingReview) {
+      
+      fetch(`/api/reviews/${editingReview}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: newReview, artist_id: selectedArtistId }),
       })
-      .catch((error) => {
-        console.error("Error adding review:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Review updated:", data);
+          setEditingReview(null); 
+          fetchReviews();
+        })
+        .catch((error) => {
+          console.error("Error updating review:", error);
+        });
+    } else {
+      
+      fetch("/api/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: newReview, artist_id: selectedArtistId }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Review added:", data);
+          setNewReview("");
+          setSelectedArtistId("");
+          fetchReviews();
+        })
+        .catch((error) => {
+          console.error("Error adding review:", error);
+        });
+    }
+  };
+
+  const handleEditReview = (reviewId) => {
+    
+    const reviewToEdit = reviews.find((review) => review.id === reviewId);
+    if (reviewToEdit) {
+      setEditingReview(reviewToEdit.id);
+      setNewReview(reviewToEdit.content);
+      setSelectedArtistId(reviewToEdit.artist_id);
+    }
+  };
+
+  const handleDeleteReview = (reviewId) => {
+    if (window.confirm("Are you sure you want to delete this review?")) {
+      fetch(`/api/reviews/${reviewId}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Review deleted:", data);
+          fetchReviews();
+        })
+        .catch((error) => {
+          console.error("Error deleting review:", error);
+        });
+    }
   };
 
   return (
@@ -77,15 +126,33 @@ function Reviews() {
           </option>
         ))}
       </select>
-      <button onClick={handleAddReview}>Add Review</button>
+      <button onClick={handleAddReview}>{editingReview ? "Update Review" : "Add Review"}</button>
       <h2>Reviews</h2>
       <ul>
-        {reviews.map((review, index) => (
-          <li key={index}>{review.content}</li>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            {editingReview === review.id ? (
+              <div>
+                <input type="text" value={newReview} onChange={(e) => setNewReview(e.target.value)} />
+                <select value={selectedArtistId} onChange={(e) => setSelectedArtistId(e.target.value)}>
+                  <option value="">Select an artist</option>
+                  {artists.map((artist) => (
+                    <option key={artist.id} value={artist.id}>
+                      {artist.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            ) : (
+              review.content
+            )}
+            <button onClick={() => handleEditReview(review.id)}>Edit</button>
+            <button onClick={() => handleDeleteReview(review.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
 
-export default withNavbar (Reviews);
+export default withNavbar(Reviews);
